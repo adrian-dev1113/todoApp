a8e3f61bf1e8c24e972407f536d5f0f3f1ad986b
Aubacca/hantan <aubacca@gmail.com>
2018-03-31 17:32:43 +0200

    Added end point to update and delete a todo.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Joi = require('joi');
 // Application data.
 let todoList = require('./data/todos.json');
 
+const baseUrl = "/api/v1";
+
 //
 // Application initializations.
 const app = express();
@@ -32,9 +34,9 @@ app.set('port', process.env.PORT || 3000);
 // Application Routing.
 app.get("/", (req, res) => res.send("Hello World, hello anybody else .."));
 
-app.get("/api/v1/notes", (req, res) => res.send(notes));
+app.get(baseUrl + "/notes", (req, res) => res.send(notes));
 
-app.get("/api/v1/notes/:noteId", (req, res) => {
+app.get(baseUrl * "/notes/:noteId", (req, res) => {
   const findOne = notes.find(note => note.id === parseInt(req.params.noteId));
   if (!findOne) {
     res
@@ -45,46 +47,59 @@ app.get("/api/v1/notes/:noteId", (req, res) => {
   res.send(findOne);
 });
 
-app.get("/api/v1/todos", (req, res) => {
-  let foundTodos = todoList;
-  if (req.query.done) {
-    foundTodos = foundTodos.filter(todo => todo.done === JSON.parse(req.query.done));
-  }
-  res.send(foundTodos)
-});
-
-app.get("/api/v1/todos/:id", (req, res) => {
-//  const findOne = todoList.filter(todo => todo._id === req.params.id);
-  const findOne = todoList.find(todo => todo._id === req.params.id);
-  console.log(findOne);
-  if (!findOne) {
-    return res.status(404).send(`The todo with ID ${req.params.id} was not found!`);
-  }
-  console.log('found!');
-  res.send(findOne);
-});
-
-app.get("/api/v1/todos-open", (req, res) => {
-  const findOne = todoList.filter(todo => todo.done === false);
-  res.send(findOne);
-});
-
 
 // @todo
-// Add a todo.
-app.post("/api/v1/todos", (req, res) => {
+// Todo utils.
+validateTodo = function (todo) {
   // Validation
   const schemaTodo = Joi.object().keys({
     todo: Joi.string().min(3).max(200).required(),
     remark: Joi.string().max(200),
     public: Joi.boolean().required()
   });
-  const result = Joi.validate(req.body, schemaTodo);
-  const {error} = Joi.validate(req.body, schemaTodo);
-//  console.log(result);
-  console.log(error);
-  if (result.error) {
-    return res.status(400).send(result.error.details[0].message);
+  //
+  return Joi.validate(todo, schemaTodo);
+}
+
+findById = function (todoId) {
+  return todoList.find(todo => todo._id === todoId);
+}
+
+// @todo
+// Get all todos.
+app.get(baseUrl + "/todos", (req, res) => {
+  let foundTodos = todoList;
+  if (req.query.done) {
+    foundTodos = foundTodos.filter(todo => todo.done === JSON.parse(req.query.done));
+  }
+  //
+  res.send(foundTodos)
+});
+
+// @todo
+// Get one specific todo.
+app.get(baseUrl + "/todos/:id", (req, res) => {
+  const findOne = findById(req.params.id);
+  if (!findOne) {
+    return res.status(404).send(`The todo with ID ${req.params.id} was not found!`);
+  }
+  res.send(findOne);
+});
+
+// @todo
+// Get all open todos.
+app.get(baseUrl + "/todos-open", (req, res) => {
+  const findTodosDone = todoList.filter(todo => todo.done === false);
+  res.send(findTodosDone);
+});
+
+// @todo
+// Add a todo.
+app.post(baseUrl + "/todos", (req, res) => {
+  // Validation
+  let {error} = validateTodo(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
   //
   const newTodo = {
@@ -100,10 +115,46 @@ app.post("/api/v1/todos", (req, res) => {
 });
 
 // @todo
-// Update a note.
+// Update a todo.
+app.put(baseUrl + "/todos/:id", (req, res) => {
+  // Find the todo to be updated in the list.
+  const findOne = findById(req.params.id);
+  if (!findOne) {
+    return res.status(404).send(`The todo with ID ${req.params.id} was not found!`);
+  }
+  //
+  // Validation updated todo values.
+  const {error} = validateTodo(req.body)
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  // Get indox of todo in list.
+  const todoIndex = todoList.findIndex(todo => todo._id === req.params.id);
+  // Update todo with new data.
+  todoList[todoIndex].todo = req.body.todo;
+  todoList[todoIndex].remark = req.body.remark;
+  todoList[todoIndex].public = req.body.public;
+  //
+  // Set back the updated todo.
+  res.send(todoList[todoIndex]);
+});
 
 // @todo
-// Delete a note.
+// Delete a todo.
+app.delete(baseUrl + "/todos/:id", (req, res) => {
+  // Find the todo to be updated.
+  const findOne = findById(req.params.id);
+  if (!findOne) {
+    return res.status(404).send(`The todo with ID ${req.params.id} was not found!`);
+  }
+  //
+  // Delete todo from list.
+  const index = todoList.indexOf(findOne);
+  todoList.splice(index, 1);
+  //
+  // Send back the deleted todo.
+  res.send(findOne);
+});
 
 // @todo
 // Add input validation.

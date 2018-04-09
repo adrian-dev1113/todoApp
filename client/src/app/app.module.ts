2872ac1ef454c96b32e7decf21dcceb66ca25753
Aubacca/hantan <aubacca@gmail.com>
2018-04-09 21:11:13 +0200

    Added additional client libs.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailComponent
   ],
   imports: [
     BrowserModule,

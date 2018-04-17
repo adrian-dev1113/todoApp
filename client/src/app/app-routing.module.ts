05f3526331758626cd36a76e55b7dc1c7060284b
Aubacca/hantan <aubacca@gmail.com>
2018-04-17 18:11:13 +0200

    Added router.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: TodoListComponent },
+  { path: 'details/:id', component: TodoDetailComponent },
+  { path: '**', component: TodoListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

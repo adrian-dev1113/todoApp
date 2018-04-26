fb65b00775ecaa93d6803687386dbc0cc3194500
Aubacca/hantan <aubacca@gmail.com>
2018-04-26 21:05:32 +0200

    Add form element.

--- client/src/app/todo/todo-detail/todo-detail.component.ts
+++ client/src/app/todo/todo-detail/todo-detail.component.ts
@@ -2,13 +2,27 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { TodoService } from "../../services/todo.service";
 
+export class Todo {
+  constructor(
+    public _id: number,
+    public toto: string,
+    public done: boolean,
+    public remark?: string
+  ) {}
+}
+
 @Component({
   selector: "app-todo-detail",
   templateUrl: "./todo-detail.component.html",
   styleUrls: ["./todo-detail.component.css"]
 })
 export class TodoDetailComponent implements OnInit {
-  public todoItem: any;
+  public todoItem: any = {
+    todo: "",
+    remark: "",
+    public: false,
+    done: false
+  };
 
   constructor(
     private route: ActivatedRoute,
@@ -24,7 +38,9 @@ export class TodoDetailComponent implements OnInit {
     });
   }
 
-  onSubmit (formsData) {
+  onSubmit(formsData) {
     console.log(formsData);
+    console.log("formsData.value:", formsData.value);
+    console.log("todoItem:", this.todoItem);
   }
 }

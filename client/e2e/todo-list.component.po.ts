f50f39f33ffbc21a8f62177564d581a51611f406
Aubacca/hantan <aubacca@gmail.com>
2018-04-27 15:22:34 +0200

    Added page object.

---
+++ client/e2e/todo-list.component.po.ts
@@ -0,0 +1,20 @@
+﻿import { browser, by, element } from 'protractor';
+
+export class TodoListPage {
+
+    get() {
+        browser.get('');
+    };
+
+    gettodoList() {
+        return element.all(by.css('mat-list > mat-list-item'));
+    } 
+
+    clickTodoItemTodoLink(rowIndex1) {
+        return this.gettodoList().get(rowIndex1).element(by.css('h4 > a')).click();
+    };
+
+    clickA0 (rowIndex1) {
+        return this.gettodoList().get(rowIndex1).element(by.css('li.todo-list-item > a')).click();
+    };
+};

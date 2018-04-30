e6a62fa1d4203f2ab77725cd8c6cce389719f0aa
adrian-dev1113 <adrianborowski1113@gmail.com>
2018-04-30 16:30:15 +0200

    Added delete methode and refactored code.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,23 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
 import { TodoService } from './services/todo.service';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent,
-    TodoDetailComponent
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoDetailComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -27,7 +22,7 @@ import { TodoService } from './services/todo.service';
     MaterialModule,
     HttpClientModule
   ],
-  providers: [ TodoService ],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/todo/todo-list/todo-list.component.ts
+++ client/src/app/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+import { Todo } from '../../models/todo.model';
+import { TodoService } from './../../services/todo.service';
 import { TodoPropertyChange } from './TodoPropertyChange';
-import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,11 +10,22 @@ import { TodoService } from '../../services/todo.service';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-
-  todoList: any[];
+  private _todoList: Todo[];
   constructor(private _todoService: TodoService) {}
 
   ngOnInit() {
-    this._todoService.find().subscribe(data => this.todoList = data);
+    this._todoService.find().subscribe(data => (this._todoList = data));
+  }
+
+  get todoList(): Todo[] {
+    return this._todoList;
+  }
+
+  deleteItem(todo: Todo) {
+    console.log('todo: ', todo);
+    this._todoService.delete(todo).subscribe(data => {
+      console.log('todo deleted: ', todo);
+      this._todoService.find().subscribe(data => (this._todoList = data));
+    });
   }
 }

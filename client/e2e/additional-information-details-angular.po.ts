58e31f4d9fde5e2f97d26507004350e67e97bf43
adrian-dev1113 <adrianborowski1113@gmail.com>
2018-04-30 11:45:16 +0200

    Added sample page objects.

---
+++ client/e2e/additional-information-details-angular.po.ts
@@ -0,0 +1,59 @@
+﻿import { browser, by, element } from "protractor";
+
+export class additionalInformationDetailsAngularPo {
+  get = function() {
+    browser.get("");
+  };
+
+  taskDataRequestDetailsRequestNumberLink = element(
+    by.exactBinding(" taskData.requestDetails.requestNumber | markEmpty ")
+  );
+
+  taskDataRequestDetailsRequestReason = element(
+    by.exactBinding(" taskData.requestDetails.requestReason | markEmpty ")
+  );
+
+  taskDataRequestDetailsRequestTypeName = element(
+    by.exactBinding(" taskData.requestDetails.requestType?.name | markEmpty ")
+  );
+
+  taskDataRequestDetailsRequestPriorityLabel = element(
+    by.exactBinding(
+      " taskData.requestDetails.requestPriority?.label | markEmpty "
+    )
+  );
+
+  taskDataDetailViewCountry = element(
+    by.exactBinding(" taskData.detailView?.country | markEmpty ")
+  );
+
+  taskDataRequestDetailsRequestPhaseLabel = element(
+    by.exactBinding(" taskData.requestDetails.requestPhase?.label | markEmpty ")
+  );
+
+  taskDataDetailViewTargetBoADeploymentDate = element(
+    by.exactBinding(
+      " taskData.detailView?.targetBoADeploymentDate | rocheDate | markEmpty "
+    )
+  );
+
+  taskDataRequestDetailsRequestStatusLabel = element(
+    by.exactBinding(
+      " taskData.requestDetails.requestStatus?.label | markEmpty "
+    )
+  );
+
+  clickFgMaterialNumberLink = function(rowIndex1) {
+    this.taskDataRelatedFgs
+      .get(rowIndex1)
+      .element(by.exactBinding(" fg.materialNumber "))
+      .click();
+  };
+
+  clickPpmMaterialNumberLink = function(rowIndex1) {
+    this.taskDataRelatedPpms
+      .get(rowIndex1)
+      .element(by.exactBinding(" ppm.materialNumber "))
+      .click();
+  };
+}
---
+++ client/e2e/additional-information-details.po.ts
@@ -0,0 +1,81 @@
+﻿import { browser, by, element } from "protractor";
+
+export class additionalInformationDetailsPo {
+  get = function() {
+    browser.get("");
+  };
+
+  taskDataRequestDetailsRequestNumberLink = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-5 > div:nth-of-type(2) > a"
+    )
+  );
+
+  taskDataRequestDetailsRequestReason = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-5 > div:nth-of-type(4)"
+    )
+  );
+
+  taskDataRequestDetailsRequestTypeName = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-5 > div:nth-of-type(6)"
+    )
+  );
+
+  taskDataRequestDetailsRequestPriorityLabel = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-4 > div:nth-of-type(2)"
+    )
+  );
+
+  taskDataDetailViewCountry = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-4 > div:nth-of-type(4)"
+    )
+  );
+
+  taskDataRequestDetailsRequestPhaseLabel = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-4 > div:nth-of-type(6)"
+    )
+  );
+
+  taskDataDetailViewTargetBoADeploymentDate = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-3 > div:nth-of-type(2)"
+    )
+  );
+
+  taskDataRequestDetailsRequestStatusLabel = element(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.col-md-3 > div:nth-of-type(4)"
+    )
+  );
+
+  taskDataRelatedFgs = element.all(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12.mt-3 > div:nth-of-type(2) > div > span"
+    )
+  );
+
+  clickFgMaterialNumberLink = function(rowIndex1) {
+    this.taskDataRelatedFgs
+      .get(rowIndex1)
+      .element(by.css("a"))
+      .click();
+  };
+
+  taskDataRelatedPpms = element.all(
+    by.css(
+      "div > div.mb-4 > div.row > div.col-12:nth-of-type(5) > div:nth-of-type(2) > div > span"
+    )
+  );
+
+  clickPpmMaterialNumberLink = function(rowIndex1) {
+    this.taskDataRelatedPpms
+      .get(rowIndex1)
+      .element(by.css("a"))
+      .click();
+  };
+}
